6cd00b1610e0f21f0e3c44be6f967fb8e84ead61
Jose458Rodrigo <119265068+Jose458Rodrigo@users.noreply.github.com>
2024-11-12 20:58:39 -0600

    Add files via upload

---
+++ reserva1.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-11-2024 a las 05:13:58
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `reserva`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva1`
+--
+
+CREATE TABLE `reserva1` (
+  `ID_RESERVA` int(11) NOT NULL,
+  `NOMBRE` text NOT NULL,
+  `FECHA_RESERVA` date NOT NULL,
+  `ESTADO_RESERVA` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reserva1`
+--
+ALTER TABLE `reserva1`
+  ADD PRIMARY KEY (`ID_RESERVA`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
